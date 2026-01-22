a5353fe3ba0ebd8a1ac7f2b1d412b002d1b0e73b
Liao Yantong <e1484072@u.nus.edu>
2026-01-22 17:48:52 +0800

    Complete Level 0: chatbot rename and greeting

---
+++ src/main/java/Stewie.java
@@ -0,0 +1,10 @@
+public class Stewie {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Well well well. I am Stewie Griffin.");
+        System.out.println("Do enlighten me — what do you bring me today?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Very well. Run along now. I have schemes to attend to.");
+        System.out.println("____________________________________________________________");
+    }
+}
