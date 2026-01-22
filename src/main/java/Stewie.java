5f1de9eaf77484afe518638f6cfc702bf510e4f4
Liao Yantong <e1484072@u.nus.edu>
2026-01-22 17:59:53 +0800

    Add support for echo

--- src/main/java/Stewie.java
+++ src/main/java/Stewie.java
@@ -1,8 +1,42 @@
+import java.util.Scanner;
+
 public class Stewie {
+
     public static void main(String[] args) {
+        new Stewie().run();
+    }
+
+    public void run() {
+        printGreeting();
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            echo(input);
+            input = scanner.nextLine();
+        }
+
+        printFarewell();
+    }
+
+
+    private void printGreeting() {
         System.out.println("____________________________________________________________");
-        System.out.println("Well well well. I am Stewie Griffin.");
-        System.out.println("Do enlighten me — what do you bring me today?");
+        System.out.println("Well well well. I am Stewie.");
+        System.out.println("Do enlighten me — what insignificant problem do you bring today?");
+        System.out.println("____________________________________________________________");
+    }
+
+
+    private void echo(String input) {
+        System.out.println("____________________________________________________________");
+        System.out.println(input);
+        System.out.println("____________________________________________________________");
+    }
+
+
+    private void printFarewell() {
         System.out.println("____________________________________________________________");
         System.out.println("Very well. Run along now. I have schemes to attend to.");
         System.out.println("____________________________________________________________");
